95fe99bc70cc6bfdaf312178caf73e0078fccae8
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-20 13:08:10 +0330

    Clean Up process done

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 // 05185b5004b3b74dc250d1bce34a1d60
@@ -8,20 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
