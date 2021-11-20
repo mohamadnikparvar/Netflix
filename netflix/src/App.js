f170814a2b84d5a2e60b82fa242af76112f451eb
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-20 13:06:35 +0330

    React project created

---
+++ netflix/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+// 05185b5004b3b74dc250d1bce34a1d60
+// netflix-e429f.web.app
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
